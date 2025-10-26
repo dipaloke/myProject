c58847f866ba44791b57a3abbe5837da00e2299a
Dipaloke <dipalokebiswas96@gmail.com>
2025-10-26 13:34:36 +0600

    practiced middleware change by logging ip address, host etc.

--- myapp/middleware.py
+++ myapp/middleware.py
@@ -3,6 +3,10 @@ class LoggingMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+
+        # logging ip
+        print(f"The ip address is: {request.META.get('REMOTE_ADDR')}, from browser: {request.META.get('HTTP_USER_AGENT')}, host: {request.META.get('HTTP_HOST')} ")
+        # method logging
         print(f"This is LoggingMiddleware: {request.method} request for '{request.path}'")
         response = self. get_response(request)
         return response
